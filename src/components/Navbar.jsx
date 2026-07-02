2d92fe18b214257b9b285500ad0288111a1b0c35
AbubakrAbbas <168645873+AbubakrAbbas@users.noreply.github.com>
2026-07-02 15:15:04 +0500

    Add active page highlighting to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 import "../styles/Navbar.css";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div className="navbar">
-                <div className="navbar-brand">
-                    <Link to="/">Movie App</Link>
-                </div>
-                <div className="navbar-links">
-                    <Link to="/" className='nav-link'>Home</Link>
-                    <Link to="/favorite" className='nav-link'>Favorite  </Link>
-                </div>
-            </div>
-        </nav>)
-}
+  return (
+    <nav>
+      <div className="navbar">
+        <div className="navbar-brand">
+          <Link to="/">Movie App</Link>
+        </div>
+        <div className="navbar-links">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/favorite" className="nav-link">
+            Favorite{" "}
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
